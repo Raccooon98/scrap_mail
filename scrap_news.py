1612917addef0c5c10dafd386c2d161dfb147946
Raccooon98 <redbull1171@gmail.com>
2023-11-10 15:37:39 +0900

    인코딩에 맞게 양식 변경

--- scrap_news.py
+++ scrap_news.py
@@ -2,9 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 
-header={'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
 url= "http://rscanner.ndmi.go.kr/scanning/negative_news_weekly.php"
 
-response= requests.get(url,headers=header).text
-soup = BeautifulSoup(response,"html.parser")
+response= requests.get(url,headers=header).content
+response = response.decode('utf-8')
+soup = BeautifulSoup(response,'html.parser')
 
+
+print(soup)
